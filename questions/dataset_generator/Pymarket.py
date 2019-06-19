f58c1f34a581e7b5fe7e9398eecc421e2b811ccc
andrsGutirrz <andres.gutierrez.arcia@gmail.com>
2019-06-19 00:01:55 -0600

    pymarket.csv generated

--- questions/dataset_generator/Pymarket.py
+++ questions/dataset_generator/Pymarket.py
@@ -4,23 +4,51 @@ This script will create a large dataset about Pymarket
 import pandas as pd
 import numpy as np
 
-df_short = pd.DataFrame({
-    'Tom': [122.49,34.22,123.0],
-    'Joe': [13.49,12.22,577.0],
-    'Charlie': [741.49,0,0],
-})
+cols = ['Tom', 'Joe', 'Charlie']
+total_records = 1000
+total_nans_whole_row = 45
+total_nans_cols = 154
+total_limit = 21
+limit = 10
 
-df_short.to_csv("pymarket.csv")
+def fill_nan(df):
+    """
+    This function adds some nans for the whole row of the df
+    you can verify the number of nans by: df.isnull().sum(axis = 0)
+    """
+    for _ in range(total_nans_whole_row):
+        df.iloc[np.random.randint(low=0, high=total_records)] = np.nan
+
+
+def fill_nan_by_colum(df):
+    """
+    This function adds some nans for some columns of the df
+    you can verify the number of nans by: df.isnull().sum(axis = 0)
+    """
+    for _ in range(total_nans_cols):
+        df.iloc[np.random.randint(low=0, high=total_records)][np.random.randint(0, len(cols))] = np.nan
+
+
+def fill_less_limit(df, limit):
+    """
+    This function adds for each colum values under limit
+    """
+    for _ in range(total_limit):
+        df.iloc[np.random.randint(low=0, high=total_records)] = [np.round(np.random.uniform(
+            0, limit), decimals=2) for i in range(len(cols))]
+
 
 def generate_pymarket():
-    total_records = 1000
-    cols = ['Tom','Joe','Charlie']
-    data = np.round(np.random.uniform(0,500,size=(total_records,len(cols))),decimals=2)
-    df = pd.DataFrame(columns=cols,data=data)
-    pass
+    data = np.round(np.random.uniform(0, 500, size=(total_records, len(cols))), decimals=2)
+    idx = pd.date_range(end='07/12/2019', freq='D', periods=total_records)
+    df = pd.DataFrame(columns=cols, data=data, index=idx)
+    fill_nan(df)
+    fill_nan_by_colum(df)
+    fill_less_limit(df,limit)
+    df.to_csv("pymarket.csv")
+
 
 if __name__ == '__main__':
     generate_pymarket()
-    print("PyMarket generated")
+    print("PyMarket.csv generated")
     pass
-# np.round(np.random.uniform(0,500,size=(3,3)),decimals=2)
